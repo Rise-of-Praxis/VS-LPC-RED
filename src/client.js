c8852a52b89f317af6993180515d10a5e57fb831
Dino Gambone <dmgambone@linearsoftwaresolutions.com>
2021-10-12 21:43:57 -0400

    Adding an Output Channel for debugging Fixed memory leak issue

--- src/client.js
+++ src/client.js
@@ -9,6 +9,9 @@ class RemoteEditorClient
 		this.#protocol = getCommandProtocol('rise-of-praxis', options);
 	}
 
+	/**
+	 * The scheme to use in {@link Uri} for links
+	 */
 	get scheme() { return 'lpc-remote-ed' }
 
 	/**
@@ -18,6 +21,7 @@ class RemoteEditorClient
 
 	/**
 	 * Generates a Uri that can be used to access a path
+	 * 
 	 * @param {string} path The path to access
 	 * @returns {Uri}
 	 */
@@ -25,6 +29,7 @@ class RemoteEditorClient
 
 	/**
 	 * Creates a {@link File} or {@link Directory} for the file information provided
+	 * 
 	 * @param {FileInfo} fileInfo The object containing the file information
 	 * @returns {File | Directory}
 	 */
@@ -40,7 +45,8 @@ class RemoteEditorClient
 	}
 
 	/**
-	 * 
+	 * Reads the content of a directory
+	 *
 	 * @param {string} path The path to get the directory listing for
 	 * @returns {Promise}
 	 */
@@ -59,6 +65,9 @@ class RemoteEditorClient
 		return directory;
 	}
 	
+	/** 
+	 * Creates a new directory
+	 */
 	async createDirectory(path) {
 		return this.#protocol.mkdir(path);
 	}
@@ -70,10 +79,11 @@ class RemoteEditorClient
 	 */
 	async who()
 	{
-		return await this.#protocol.who();
+		return this.#protocol.who();
 	}
 
 	/**
+	 * Gets information about a file or directory
 	 * 
 	 * @param {string} path The path to get the file information
 	 * @returns {Promise<FileInfo>}
@@ -81,34 +91,65 @@ class RemoteEditorClient
 	async getFileInfo(path)
 	{
 		const response = await this.#protocol.info(path);
+		response.uri = this.getFileUri(path);
 
 		return response;
 	}
 
+	/**
+	 * Reads a file
+	 * 
+	 * @param {string} path The path of the file to read 
+	 * @returns {Buffer} The file content
+	 */
 	async readFile(path)
 	{
-		return await this.#protocol.get(path);
+		return this.#protocol.get(path);
 	}
 
+	/**
+	 * Writes a file
+	 * 
+	 * @param {string} path The path of the file to be written to
+	 * @param {Buffer} content The file content to write
+	 * @param {object} options Options used for writing the file
+	 */
 	async writeFile(path, content, options)
 	{
-		const response = await this.#protocol.post(path, content, options);
-
-		return response;
+		this.#protocol.post(path, content, options);
 	}
 
+	/**
+	 * Deletes a file
+	 * 
+	 * @param {string} path The path of the file to delete
+	 * @param {object} options Options used for deleting the file
+	 */
 	async deleteFile(path, options)
 	{
-		return this.#protocol.rm(path, options);
+		this.#protocol.rm(path, options);
 	}
 
-	async deleteDirectory(path, options)
+	/**
+	 * Deletes a directory
+	 * 
+	 * @param {string} path The path of the directory to delete
+	 * @param {object} options Options used for deleting the directory
+	 */
+	 async deleteDirectory(path, options)
 	{
-		return this.#protocol.rmdir(path, options);
+		this.#protocol.rmdir(path, options);
 	}
 
-	async copy(oldPath, newPath, options) {
-		return this.#protocol.cp(oldPath, newPath, options);
+	/**
+	 * Copies a file from one location to another location
+	 * 
+	 * @param {string} oldPath The path of the source file
+	 * @param {string} newPath The path of the target file
+	 * @param {object} options Options used for copying the file
+	 */
+	 async copy(oldPath, newPath, options) {
+		this.#protocol.cp(oldPath, newPath, options);
 	}
 }
 
