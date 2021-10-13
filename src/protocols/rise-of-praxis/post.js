14e5b4f63557bbca0e0577054b28621576f8441d
Dino Gambone <dmgambone@linearsoftwaresolutions.com>
2021-10-13 00:05:47 -0400

    Fixing bug with large file uploads

--- src/protocols/rise-of-praxis/post.js
+++ src/protocols/rise-of-praxis/post.js
@@ -1,5 +1,6 @@
 const RemoteEditorRequest = require('./request');
 const { FileSystemError } = require('vscode');
+const { rejects } = require('assert');
 
 
 module.exports = async (connectionOptions, path, content, options) =>
@@ -7,23 +8,43 @@ module.exports = async (connectionOptions, path, content, options) =>
 	const size = content ? content.length : 0;
 	const request = new RemoteEditorRequest(connectionOptions);
 
+	const chunkSize = 1024;
+
 	const uploadFile = (startResponse, connection) =>
 	{
-		const chunkSize = 1024;
-		if (startResponse.statusCode !== "200"
-			|| size === 0)
-			return;
-
-		let remainingData = content.toString();
-		let remaining = size;
-		while (remaining > 0)
+		return new Promise((resolve, reject) =>
 		{
-			const chunk = remainingData.substr(0, chunkSize);
-			connection.write(chunk);
+			if (startResponse.statusCode !== "200"
+				|| size === 0)
+				resolve();
 
-			remainingData = remainingData.substr(chunkSize);
-			remaining -= chunkSize;
-		}
+			const uploadResponse = (buffer) =>
+			{
+				const data = buffer.toString();
+				if(data.startsWith("1"))
+					return;
+
+				if(data.startsWith("2"))
+				{
+					connection.off('data', uploadResponse);
+					resolve();
+					return;
+				}
+
+				reject(data.substr(4));
+			};
+
+			connection.on('data', uploadResponse);
+
+			let remainingData = content.toString();
+			while (remainingData.length > 0)
+			{
+				const chunk = remainingData.substr(0, chunkSize);
+				connection.write(chunk);
+
+				remainingData = remainingData.substr(chunkSize);
+			}
+		});
 	};
 
 	const response = await request.send(`post ${path} ${size}\n`, uploadFile);
