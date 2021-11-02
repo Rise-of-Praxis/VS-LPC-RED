5bd0766cc6cb4159e6f122375a07800c7beaa2f4
Dino Gambone <dmgambone@linearsoftwaresolutions.com>
2021-11-02 12:25:03 -0400

    Removed 'start.js' as it's obsolete now

--- src/extension-commands/index.js
+++ src/extension-commands/index.js
@@ -1,11 +1,9 @@
-const startRemoteEditor = require('./start');
 const createNewWorkspace = require('./create-new-workspace');
 const registerFileSystem = require('./register-file-system');
 const addMyRealms = require('./add-my-realms');
 const registerLanguageFeatures = require('./register-language-features');
 
 module.exports = {
-	startRemoteEditor,
 	createNewWorkspace,
 	registerFileSystem,
 	registerLanguageFeatures,
