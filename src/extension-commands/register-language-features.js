f6fe8a923d8506d8c53847749c0da4072a92f1a6
Dino Gambone <dmgambone@yahoo.com>
2023-12-18 08:40:30 -0500

    Update grammar to handle comments prior to statements better. Fixed relative path resolution

--- src/extension-commands/register-language-features.js
+++ src/extension-commands/register-language-features.js
@@ -3,12 +3,8 @@ const { LPCLanguageProvider } = require("../lpc");
 const { getConfiguration } = require("../utilities/configuration");
 
 module.exports = async(context) => {
-	const lpcSyntaxDiagnostics = languages.createDiagnosticCollection("lpc-syntax");
-	context.subscriptions.push(lpcSyntaxDiagnostics);
-	
 	const config = {
-		...getConfiguration(),
-		lpcSyntaxDiagnostics
+		...getConfiguration()
 	}
 	const languageProvider = new LPCLanguageProvider();
 	languageProvider.register(context, config);
