1ea31756e5431a093a9344a0fde4cdca293153d8
Dino Gambone <dmgambone@linearsoftwaresolutions.com>
2024-02-09 09:58:10 -0500

    Adding better language support checks

--- src/extension-commands/register-language-features.js
+++ src/extension-commands/register-language-features.js
@@ -1,10 +1,15 @@
 const { languages } = require("vscode");
 const { LPCLanguageProvider } = require("../lpc-lang");
+const { getConfiguration } = require("../utilities/configuration");
 
 module.exports = async(context) => {
 	const lpcSyntaxDiagnostics = languages.createDiagnosticCollection("lpc-syntax");
 	context.subscriptions.push(lpcSyntaxDiagnostics);
 	
+	const config = {
+		...getConfiguration(),
+		lpcSyntaxDiagnostics
+	}
 	const languageProvider = new LPCLanguageProvider();
-	languageProvider.register(context, lpcSyntaxDiagnostics);
+	languageProvider.register(context, config);
 }
